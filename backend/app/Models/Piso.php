23d5b5c1861f1556b1ed88ff3662bfb91117a311
Dh4r10 <marzchelo1@gmail.com>
2024-10-07 14:42:34 -0500

    piso table

---
+++ backend/app/Models/Piso.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Piso extends Model
+{
+    use HasFactory;
+}
