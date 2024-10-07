7251a4c1df00dc96937080120fc7637575b35b25
Dh4r10 <marzchelo1@gmail.com>
2024-10-07 15:50:04 -0500

    Detalle pedido table

---
+++ backend/database/migrations/2024_10_07_203914_create_detalle_pedido_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->integer('cantidad');
+            $table->decimal('precio', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_pedido');
+    }
+};
